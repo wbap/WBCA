f532d2c1515bdf19f2390f4734a603b7a2fdb5b0
businy <mochorusingers@gmail.com>
2017-12-26 23:35:55 +0900

    Added thalamus info

--- Codes/conn.py
+++ Codes/conn.py
@@ -12,10 +12,10 @@ def hasNumbers(inputString):
 
 
 def loadMatrix():
-    csv_file = 'DataAnalysis/s4_ipsi_nnls_171203.csv'
+    csv_file = '../DataAnalysis/s4_ipsi_nnls_171203.csv'
     df = pd.read_csv(csv_file, index_col=0, header=0)
 
-    fffb_file = 'DataAnalysis/fffb_dawd.csv'
+    fffb_file = '../DataAnalysis/fffb_dawd.csv'
     fffb_df = pd.read_csv(fffb_file, header=0)
 
     for key, row in fffb_df.iterrows():
@@ -24,13 +24,13 @@ def loadMatrix():
 
     acronym_list = np.sort(np.unique(list(df.index) + list(df.columns)))
 
-    df.to_csv('Release/conn_matrix.csv', header=True, index=True)
+    df.to_csv('../Release/conn_matrix.csv', header=True, index=True)
 
     return acronym_list
 
 
 def hierarchy(acronym_list):
-    csv_file = 'DataAnalysis/s2_171020.csv'
+    csv_file = '../DataAnalysis/s2_171020.csv'
     df = pd.read_csv(csv_file, index_col=0, header=0)
 
     parent_list = ['Cerebellum', 'Isocortex', 'Hippocampal formation', 'Olfactory areas', 'Cortical subplate',
@@ -47,25 +47,25 @@ def hierarchy(acronym_list):
                 temp_list.append(reg)
             hierarchy_list.append(ac)
 
-    csv_file1 = 'DataAnalysis/finale_matrix.csv'
+    csv_file1 = '../DataAnalysis/finale_matrix.csv'
     df1 = pd.read_csv(csv_file1, header=0)
 
     df1.insert(loc=0, column='Region', value=temp_list)
     df1.insert(loc=0, column='Acronym', value=hierarchy_list)
-    df1.to_csv('Release/port_size_matrix.csv', index=0)
+    df1.to_csv('../Release/port_size_matrix.csv', index=0)
 
-    with open('Release/hierarchy.txt', 'w') as output_file:
-        with open('Release/port_size_matrix.csv', 'r') as input_file:
+    with open('../Release/hierarchy.txt', 'w') as output_file:
+        with open('../Release/port_size_matrix.csv', 'r') as input_file:
             [output_file.write('\t'.join(row) + '\n') for row in csv.reader(input_file)]
         # output_file.close()
 
-    df1.to_csv('Release/port_size_matrix.csv', header=True, index=True)
+    df1.to_csv('../Release/port_size_matrix.csv', header=True, index=True)
 
     return hierarchy_list
 
 
 def extractMatrix(hierarchy_list):
-    csv_file = 'Release/conn_matrix.csv'
+    csv_file = '../Release/conn_matrix.csv'
     df = pd.read_csv(csv_file, index_col=0, header=0)
 
     col_delete_list = [name for name in df.columns if name not in hierarchy_list]
@@ -80,28 +80,28 @@ def extractMatrix(hierarchy_list):
 
     acronym_list = np.sort(np.unique(list(df.index) + list(df.columns)))
 
-    df3.to_csv('Release/conn_matrix.csv', header=True, index=True)
+    df3.to_csv('../Release/conn_matrix.csv', header=True, index=True)
 
     return acronym_list
 
 
 def connectivity():
-    csv_file = 'Release/conn_matrix.csv'
+    csv_file = '../Release/conn_matrix.csv'
     df = pd.read_csv(csv_file, index_col=0, header=0)
 
-    df.to_csv('Release/conn_matrix.csv', header=True, index=True)
+    df.to_csv('../Release/conn_matrix.csv', header=True, index=True)
 
-    with open('Release/connection.txt', 'w') as my_output_file:
+    with open('../Release/connection.txt', 'w') as my_output_file:
         with open(csv_file, 'r') as my_input_file:
             [my_output_file.write('\t'.join(row) + '\n') for row in csv.reader(my_input_file)]
         # my_output_file.close()
 
 
 def blockDiagram():
-    csv_file = 'Release/conn_matrix.csv'
+    csv_file = '../Release/conn_matrix.csv'
     df = pd.read_csv(csv_file, index_col=0, header=0)
 
-    with open('Release/blockDiagram.txt', 'w') as f:
+    with open('../Release/blockDiagram.txt', 'w') as f:
         for idx in df.index:
             for col in df.columns:
                 if df.at[idx, col] > 0:
@@ -113,14 +113,14 @@ def blockDiagram():
 
 
 def region(acronym_list):
-    csv_file = 'DataAnalysis/ontology_170731.csv'
+    csv_file = '../DataAnalysis/ontology_170731.csv'
     df = pd.read_csv(csv_file, index_col=0, header=0)
 
     parent_list = ['Cerebellum', 'Isocortex', 'Hippocampal formation', 'Olfactory areas', 'Cortical subplate',
                    'Cerebral nuclei', 'Thalamus', 'Hypothalamus', 'Midbrain', 'Medulla', 'Pons']
 
     region_list = []
-    with open('Release/regions.txt', 'w') as f:
+    with open('../Release/regions.txt', 'w') as f:
         for ac, name in zip(df['acronym'], df['safe_name']):
             if ac in acronym_list:
                 f.write('{}\t{}\t{}\t{}\n'.format(ac, ac, name, name))
